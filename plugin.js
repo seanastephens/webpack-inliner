1e964becec5caa0306c407f15461a491fab9a56f
Sean Stephens <seanastephens@email.arizona.edu>
2017-03-05 19:58:39 -0700

    Rename ambiguously-named helper functions

--- plugin.js
+++ plugin.js
@@ -1,6 +1,6 @@
 const ConstDependency = require('webpack/lib/dependencies/ConstDependency');
 
-const makeLogger = (requirers, module) => message =>
+const makeQuitter = (requirers, module) => message =>
   console.log(
     'Not inlining',
     module.rawRequest,
@@ -33,63 +33,63 @@ HelloWorldPlugin.prototype.apply = function(compiler) {
 
       refMap.forEach((requirers, module) => {
 
-        const bail = makeLogger(requirers, module);
-  
+        const quitWithMessage = makeQuitter(requirers, module);
+
         if(requirers.length > 1) {
-          return bail(`Imported ${requirers.length} > 1 times.`);
+          return quitWithMessage(`Imported ${requirers.length} > 1 times.`);
         }
-  
+
         if(module.dependencies.length > 0) {
           const estimate = module.dependencies.length / 2;
-          return bail(`It has dependencies (estimated ${estimate}).`);
+          return quitWithMessage(`It has dependencies (estimated ${estimate}).`);
         }
-  
+
         const inlineCandidate = compilation.getModule(module);
         const receiver = compilation.getModule(requirers[0]);
-  
+
         if(!inlineCandidate._source) {
-          return bail('Can\'t find source of inline candidate.');
+          return quitWithMessage('Can\'t find source of inline candidate.');
         }
-  
+
         const byPosition = (a, b) => a.range[0] - b.range[0];
         const sortedDeps = receiver.dependencies.slice().sort(byPosition);
         const pairs = sortedDeps
           .map((x, i) => [x, i])
           .filter(x => x[0].type === 'cjs require')
-          .map(x => ({ 
+          .map(x => ({
             moduleIdDep: x[0],
             webpackHeaderDep: sortedDeps[x[1]-1]
           }))
           .filter(dep => dep.moduleIdDep.request === inlineCandidate.rawRequest);
-  
+
         if(pairs.length !== 1) {
           // Being extra conservative here: If the import happens twice,
           // we can't inline because that would duplicate module
           // state/side effects. If it happens zero times, well,
           // something else is wrong.
-          return bail([
+          return quitWithMessage([
             'Receiver candidate imported the inline candidate',
             `${pairs.length} times, needs to be one.`
           ].join(' '));
         }
-  
+
         const pair = pairs[0];
-  
+
         const pairIsAdjacent = pair.webpackHeaderDep.range[1] === pair.moduleIdDep.range[0] - 1;
-  
+
         if(!pairIsAdjacent) {
-          return bail(`Sanity check failed: pairIsAdjacent=${pairIsAdjacent}`);
+          return quitWithMessage(`Sanity check failed: pairIsAdjacent=${pairIsAdjacent}`);
         }
-  
+
         console.log('Inlining', module.rawRequest, 'into', requirers[0].rawRequest);
-  
+
         const range = [pair.webpackHeaderDep.range[0], pair.moduleIdDep.range[1] + 1];
-  
+
         // We need to insert a statement after 'use strict';
         const insert = inlineCandidate._source.source()
           .replace(/['"]use strict['"];/g, '')
           .replace('module.exports', 'exports');
-  
+
         const inlineModuleDefinition = [
           '(function() {',
           '"use strict";',
@@ -98,15 +98,15 @@ HelloWorldPlugin.prototype.apply = function(compiler) {
           'return exports;',
           '})()'
         ].join('\n');
-  
+
         receiver.addDependency(new ConstDependency(inlineModuleDefinition, range));
-  
+
         receiver.dependencies = receiver.dependencies.filter(dep => {
           return dep != pair.moduleIdDep && dep != pair.webpackHeaderDep;
         });
-  
+
         deadModules.add(inlineCandidate.identifier());
-  
+
       });
 
       const shouldDiscard = x => deadModules.has(x.identifier());
